0011d7ee412e3ee30d3a92a45390c464b32365fc
yash chaudhary <yash240196@gmail.com>
2018-01-22 00:18:19 +0530

    details of customer on right

--- data.js
+++ data.js
@@ -6,6 +6,7 @@ const gender = document.getElementById('gender');
 const email = document.getElementById('email');
 
 let flag='a';
+var asd;
 
 function createNode(element) {
     return document.createElement(element);
@@ -24,10 +25,14 @@ function reset() {
     }
 }
 
+
+
 function fetch_data(flag) {
 fetch("https://api.myjson.com/bins/ebwqx")
     .then(response => response.json())
     .then(data => {
+
+        asd=data;
         if(flag=='a' || flag==null){
             reset();
         var all=data;
@@ -88,3 +93,46 @@ def.addEventListener("click", () => {
     flag = 'a';
     fetch_data(flag);
 }, false);
+
+
+var det = document.getElementById('det');
+var detail = document.getElementById('detail');
+
+function new_inf() {
+    while (detail.firstChild) {
+        detail.removeChild(detail.firstChild);
+
+    }
+}
+
+function info(inf){
+        //console.log(asd);
+        var i=asd.filter(a => a.first_name +' '+ a.last_name == inf );
+      //  console.log(i,i[0].id);
+    let id_li = createNode('li');
+    if(id_li.innerHTML!=null){new_inf()}
+    id_li.innerHTML = `${i[0].id}`;
+    append(detail, id_li);
+
+    let name_li = createNode('li');
+    name_li.innerHTML = `${i[0].first_name} ${i[0].last_name}`;
+    append(detail, name_li);
+
+    let g_li = createNode('li');
+    g_li.innerHTML = `${i[0].gender}`;
+    append(detail, g_li);
+
+    let e_li = createNode('li');
+    e_li.innerHTML = `${i[0].email}`;
+    append(detail, e_li);
+}
+
+
+name.addEventListener('click',(e) => {
+        det.style.display = 'block';
+        
+      //  console.log(e.target.innerHTML);
+
+      info(e.target.innerHTML);
+})
+
